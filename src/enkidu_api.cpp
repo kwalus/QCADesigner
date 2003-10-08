6b274e0623d2ae020062ee8662dd6626d5c1ac1c
tdysart <>
2003-10-08 19:16:31 +0000

    added code to kill digital simulation if parcel queue doesn't clear up after 1K iterations

--- src/enkidu_api.cpp
+++ src/enkidu_api.cpp
@@ -5,6 +5,9 @@
 // 5 APR 2003
 
 #include "enkidu.h"
+#include "cell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 list<component*> component::componentList;
 queue<parcel*> component::parcelQ;
@@ -12,6 +15,11 @@ queue<component*> component::majQ;
 
 int component::_cycle = 0;
 bool component::_keepAlive = 0;
+static const int Mqsize = 100;
+static int Oqsize = 0;
+static int times_same = 0;
+static int times_thru = 0;
+
 // component::componentNameMap component::componentsByName;
 
 //: Run the simulation for a set period of time or until done
@@ -49,16 +57,69 @@ void component::run(int forCycles)
 		for (componentVec::iterator i = componentList.begin(); i != componentList.end(); ++i) {
 			(*i)->preTic();
 		}
+		fprintf(stderr, "done w/ preTic\n");
+		fflush(stderr);
 		bool done = false;
 		while (!done) {
 			done = true;
 			/* Process the parcels for this cycle */
 			while(!parcelQ.empty()) {
 				parcel *p = parcelQ.front();
+				fprintf(stderr, "Here10.\n");
+				    fflush(stderr);
+				    if (p->source() != NULL){
+				  printf("source node!\n");
+				  p->source()->printCellInfo();
+				  fflush(stdout);
+				}
 				p->destination()->handleParcel(p);
+				/* DEBUG CODE */
+				// if (10 == parcelQ.size())
+// 				  times_same++;
+// 				fflush(stderr);
+// 				if (times_same > 50){
+// 				  fprintf(stderr, "enkidu parcels -- 1\n");
+// 				  fflush(stderr);
+// 				    /* print out offending cell locations */
+// 				  for (int i = 0; i < parcelQ.size(); i++){
+// 				    fprintf(stderr, "Here3.\n");
+// 				    fflush(stderr);
+// 				    if (p == NULL)
+// 				      continue;
+// 				    fprintf(stderr, "Here4.\n");
+// 				    fflush(stderr);
+// 				    /*if (p->source() != NULL){
+// 				      printf("source node!\n");
+// 				      p->source()->printCellInfo();
+// 				      fflush(stdout);
+// 				      }*/
+// 				    fprintf(stderr, "Here5.\n");
+// 				    fflush(stderr);
+// 				    if (p->destination() != NULL){
+// 				      printf("dest node\n.");
+// 				      p->destination()->printCellInfo();
+// 				      fflush(stdout);
+// 				    }
+// 				    fprintf(stderr, "Here.\n");
+// 				    fflush(stderr);
+// 				  }
+// 				  exit(0);
+// 				}
+// 				Oqsize = parcelQ.size();
+				/* END DEBUG CODE */
 				p->destination()->didWorkThisCycle = 1;
 				parcelQ.pop();
+				times_thru++;
+				if (times_thru == 1000){
+				  fprintf(stderr, "Digital Simulation did not work.  Program Exiting.\n");
+				  fflush(stderr);
+				  exit(0);
+				}
+				  
+				
 			}
+			fprintf(stderr, "print me 3!\n");
+			fflush(stderr);
 			if (!majQ.empty()) {
 				done = false;
 				int Qsize = majQ.size();
@@ -66,7 +127,7 @@ void component::run(int forCycles)
 				int tries = 1;
 				while ((n != 1) && (n != 3)) {
 					if (tries > Qsize) {
-						printf("ERROR.  tries = %d\n",tries);
+						fprintf(stderr, "ERROR.  tries = %d\n",tries);
 						exit(1);
 					}
 					component * f = majQ.front();
@@ -82,6 +143,7 @@ void component::run(int forCycles)
 				f->setIsMajority(n != 1);
 				f->setIsPotentialMaj(false);
 			}
+			fprintf(stderr, "print me 2\n");
 		}
 		/* Run idle functions, if needed */
 		for (componentVec::iterator i = componentList.begin(); i != componentList.end(); ++i) {
@@ -97,6 +159,8 @@ void component::run(int forCycles)
 		}
 		/* Advance the cycle count */
 		_cycle++;
+		fprintf(stderr, "add a cycle\n");
+		fflush(stderr);
 	}
 		/* Finish */
 //	for (list<component*>::iterator i = componentList.begin(); i != componentList.end(); ++i) {

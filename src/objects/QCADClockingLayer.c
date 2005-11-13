6152be5ea0833afde4d89afa5ae79b5a99193026
nix <>
2005-11-13 01:20:42 +0000

    *** empty log message ***

---
+++ src/objects/QCADClockingLayer.c
@@ -0,0 +1,161 @@
+//////////////////////////////////////////////////////////
+// QCADesigner                                          //
+// Copyright 2002 Konrad Walus                          //
+// All Rights Reserved                                  //
+// Author: Konrad Walus                                 //
+// Email: qcadesigner@gmail.com                         //
+// WEB: http://qcadesigner.ca/                          //
+//////////////////////////////////////////////////////////
+//******************************************************//
+//*********** PLEASE DO NOT REFORMAT THIS CODE *********//
+//******************************************************//
+// If your editor wraps long lines disable it or don't  //
+// save the core files that way. Any independent files  //
+// you generate format as you wish.                     //
+//////////////////////////////////////////////////////////
+// Please use complete names in variables and fucntions //
+// This will reduce ramp up time for new people trying  //
+// to contribute to the project.                        //
+//////////////////////////////////////////////////////////
+// This file was contributed by Gabriel Schulhof        //
+// (schulhof@atips.ca).                                 //
+//////////////////////////////////////////////////////////
+// Contents:                                            //
+//                                                      //
+// The layer. This is a structure containing design     //
+// objects. The kinds of objects a layer may contain    //
+// depend on the kind of layer it is.                   //
+//                                                      //
+//////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "QCADClockingLayer.h"
+#include "QCADLayer_priv.h"
+#include "support.h"
+
+enum
+  {
+  QCAD_CLOCKING_LAYER_PROPERTY_SHOW_POTENTIAL = 1,
+  QCAD_CLOCKING_LAYER_PROPERTY_DISTANCE,
+  QCAD_CLOCKING_LAYER_PROPERTY_TILE_SIZE,
+  QCAD_CLOCKING_LAYER_PROPERTY_LAST
+  } ;
+
+static void qcad_clocking_layer_class_init (QCADDesignObjectClass *klass, gpointer data) ;
+static void qcad_clocking_layer_instance_init (QCADDesignObject *object, gpointer data) ;
+static void qcad_clocking_layer_instance_finalize (GObject *object) ;
+static void qcad_clocking_layer_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec) ;
+static void qcad_clocking_layer_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec) ;
+#ifdef GTK_GUI
+static void draw (QCADDesignObject *obj, GdkDrawable *dst, GdkFunction rop, GdkRectangle *rcClip) ;
+#endif /* def GTK_GUI */
+
+GType qcad_clocking_layer_get_type ()
+  {
+  static GType qcad_clocking_layer_type = 0 ;
+
+  if (!qcad_clocking_layer_type)
+    {
+    static const GTypeInfo qcad_clocking_layer_info =
+      {
+      sizeof (QCADClockingLayerClass),
+      (GBaseInitFunc)NULL,
+      (GBaseFinalizeFunc)NULL,
+      (GClassInitFunc)qcad_clocking_layer_class_init,
+      (GClassFinalizeFunc)NULL,
+      NULL,
+      sizeof (QCADClockingLayer),
+      0,
+      (GInstanceInitFunc)qcad_clocking_layer_instance_init
+      } ;
+
+    if ((qcad_clocking_layer_type = g_type_register_static (QCAD_TYPE_LAYER, QCAD_TYPE_STRING_CLOCKING_LAYER, &qcad_clocking_layer_info, 0)))
+      g_type_class_ref (qcad_clocking_layer_type) ;
+    }
+  return qcad_clocking_layer_type ;
+  }
+
+static void qcad_clocking_layer_class_init (QCADDesignObjectClass *klass, gpointer data)
+  {
+  G_OBJECT_CLASS (klass)->finalize     = qcad_clocking_layer_instance_finalize ;
+  G_OBJECT_CLASS (klass)->set_property = qcad_clocking_layer_set_property ;
+  G_OBJECT_CLASS (klass)->get_property = qcad_clocking_layer_get_property ;
+#ifdef GTK_GUI
+  QCAD_DESIGN_OBJECT_CLASS (klass)->draw = draw ;
+#endif /* def GTK_GUI */
+
+  g_object_class_install_property (G_OBJECT_CLASS (klass), QCAD_CLOCKING_LAYER_PROPERTY_SHOW_POTENTIAL,
+    g_param_spec_boolean ("show-potential", _("Show Potential"), _("Show potential created by the electrodes on this layer"),
+      FALSE, G_PARAM_READABLE | G_PARAM_WRITABLE)) ;
+
+  g_object_class_install_property (G_OBJECT_CLASS (klass), QCAD_CLOCKING_LAYER_PROPERTY_DISTANCE,
+    g_param_spec_double ("distance", _("Distance from layer"), _("Distance from clocking layer to show the cross-section for"),
+      -G_MAXDOUBLE, G_MAXDOUBLE, 0, G_PARAM_READABLE | G_PARAM_WRITABLE)) ;
+
+  g_object_class_install_property (G_OBJECT_CLASS (klass), QCAD_CLOCKING_LAYER_PROPERTY_TILE_SIZE,
+    g_param_spec_uint ("tile-size", _("Tile Size"), _("Resolution (n x n pixels) used to draw the potential"),
+      1, G_MAXUINT, 16, G_PARAM_READABLE | G_PARAM_WRITABLE)) ;
+
+  }
+
+static void qcad_clocking_layer_instance_init (QCADDesignObject *object, gpointer data)
+  {
+  QCADLayer *layer = QCAD_LAYER (object) ;
+
+  layer->type = LAYER_TYPE_CLOCKING ;
+  layer->default_properties = qcad_layer_create_default_properties (LAYER_TYPE_CLOCKING) ;
+  QCAD_CLOCKING_LAYER (layer)->bDrawPotential = FALSE ;
+  QCAD_CLOCKING_LAYER (layer)->z_to_draw = 0 ;
+  QCAD_CLOCKING_LAYER (layer)->tile_size = 16 ;
+  }
+
+static void qcad_clocking_layer_instance_finalize (GObject *object)
+  {G_OBJECT_CLASS (g_type_class_peek (g_type_parent (QCAD_TYPE_LAYER)))->finalize (object) ;}
+
+static void qcad_clocking_layer_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
+  {
+  QCADClockingLayer *layer = QCAD_CLOCKING_LAYER (object) ;
+
+  switch (property_id)
+    {
+    case QCAD_CLOCKING_LAYER_PROPERTY_SHOW_POTENTIAL:
+      layer->bDrawPotential = g_value_get_boolean (value) ;
+      break ;
+
+    case QCAD_CLOCKING_LAYER_PROPERTY_DISTANCE:
+      layer->z_to_draw = g_value_get_double (value) ;
+      break ;
+
+    case QCAD_CLOCKING_LAYER_PROPERTY_TILE_SIZE:
+      layer->tile_size = g_value_get_uint (value) ;
+      break ;
+    }
+  }
+
+static void qcad_clocking_layer_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+  {
+  QCADClockingLayer *layer = QCAD_CLOCKING_LAYER (object) ;
+
+  switch (property_id)
+    {
+    case QCAD_CLOCKING_LAYER_PROPERTY_SHOW_POTENTIAL:
+      g_value_set_boolean (value, layer->bDrawPotential) ;
+      break ;
+
+    case QCAD_CLOCKING_LAYER_PROPERTY_DISTANCE:
+      g_value_set_double (value, layer->z_to_draw) ;
+      break ;
+
+    case QCAD_CLOCKING_LAYER_PROPERTY_TILE_SIZE:
+       g_value_set_uint (value, layer->tile_size) ;
+      break ;
+    }
+  }
+
+#ifdef GTK_GUI
+static void draw (QCADDesignObject *obj, GdkDrawable *dst, GdkFunction rop, GdkRectangle *rcClip)
+  {QCAD_DESIGN_OBJECT_CLASS (g_type_class_peek (g_type_parent (QCAD_TYPE_CLOCKING_LAYER)))->draw (obj, dst, rop, rcClip) ;}
+#endif /* def GTK_GUI */
---
+++ src/objects/QCADClockingLayer.h
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////
+// QCADesigner                                          //
+// Copyright 2002 Konrad Walus                          //
+// All Rights Reserved                                  //
+// Author: Konrad Walus                                 //
+// Email: qcadesigner@gmail.com                         //
+// WEB: http://qcadesigner.ca/                          //
+//////////////////////////////////////////////////////////
+//******************************************************//
+//*********** PLEASE DO NOT REFORMAT THIS CODE *********//
+//******************************************************//
+// If your editor wraps long lines disable it or don't  //
+// save the core files that way. Any independent files  //
+// you generate format as you wish.                     //
+//////////////////////////////////////////////////////////
+// Please use complete names in variables and fucntions //
+// This will reduce ramp up time for new people trying  //
+// to contribute to the project.                        //
+//////////////////////////////////////////////////////////
+// This file was contributed by Gabriel Schulhof        //
+// (schulhof@atips.ca).                                 //
+//////////////////////////////////////////////////////////
+// Contents:                                            //
+//                                                      //
+// Header for the layer. This is a structure containing //
+// design objects. The kinds of objects a layer may     //
+// contain depend on the kind of layer it is.           //
+//                                                      //
+//////////////////////////////////////////////////////////
+
+#ifndef _OBJECTS_QCADClockingLayer_H_
+#define _OBJECTS_QCADClockingLayer_H_
+
+#ifdef GTK_GUI
+  #include <gtk/gtk.h>
+#endif /* def GTK_GUI */
+#include "QCADLayer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct
+  {
+  QCADLayer parent_instance ;
+  gboolean bDrawPotential ;
+  double z_to_draw ; // nm
+  int tile_size ; // pixels
+  } QCADClockingLayer ;
+
+typedef struct
+  {
+  QCADLayerClass parent_class ;
+  } QCADClockingLayerClass ;
+
+GType qcad_clocking_layer_get_type () ;
+
+#define QCAD_TYPE_STRING_CLOCKING_LAYER "QCADClockingLayer"
+#define QCAD_TYPE_CLOCKING_LAYER (qcad_clocking_layer_get_type ())
+#define QCAD_CLOCKING_LAYER(object) (G_TYPE_CHECK_INSTANCE_CAST ((object), QCAD_TYPE_CLOCKING_LAYER, QCADClockingLayer))
+#define QCAD_CLOCKING_LAYER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), QCAD_TYPE_CLOCKING_LAYER, QCADClockingLayerClass))
+#define QCAD_IS_CLOCKING_LAYER(object) (G_TYPE_CHECK_INSTANCE_TYPE ((object), QCAD_TYPE_CLOCKING_LAYER))
+#define QCAD_IS_CLOCKING_LAYER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), QCAD_TYPE_CLOCKING_LAYER))
+#define QCAD_CLOCKING_LAYER_GET_CLASS(object) (G_TYPE_INSTANCE_GET_CLASS ((object), QCAD_TYPE_CLOCKING_LAYER, QCADClockingLayerClass))
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _QCADClockingLayer_H_ */

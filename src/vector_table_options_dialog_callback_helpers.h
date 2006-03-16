2058e6f4ea44fd92cae1d7292b77a7bff694dc22
nix <>
2006-03-16 21:58:41 +0000

    Things that are in _callbacks.c but also used in vector_table_options_dialog_*.c

---
+++ src/vector_table_options_dialog_callback_helpers.h
@@ -0,0 +1,8 @@
+#ifndef _VECTOR_TABLE_OPTIONS_DIALOG_CALLBACK_HELPERS_H_
+#define _VECTOR_TABLE_OPTIONS_DIALOG_CALLBACK_HELPERS_H_
+
+#include "vector_table_options_dialog_interface.h"
+
+void vtod_reflect_number_of_vectors_changed (vector_table_options_D *dialog, VECTOR_TABLE_OPTIONS_DIALOG_DATA *dialog_data) ;
+
+#endif /* _VECTOR_TABLE_OPTIONS_DIALOG_CALLBACK_HELPERS_H_ */

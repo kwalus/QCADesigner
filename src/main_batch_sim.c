43f67179ce60cc37d228b5e1a9461088ac3b6e32
nix <>
2006-03-24 17:09:41 +0000

    Marked main_batch_sim strings for translation.

--- src/main_batch_sim.c
+++ src/main_batch_sim.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <glib.h>
+#include "intl.h"
 #include "fileio.h"
 #include "fileio_helpers.h"
 #include "design.h"
@@ -98,7 +99,7 @@ int main (int argc, char **argv)
   parse_cmdline (argc, argv, &cmdline_args) ;
 
   flush_fprintf (stderr,
-    "Simulation engine               : %s\n"
+  _("Simulation engine               : %s\n"
     "Simulation options file         : %s\n"
     "Circuit file                    : %s\n"
     "Reference simulation output file: %s\n"
@@ -109,7 +110,7 @@ int main (int argc, char **argv)
     "samples for running average     : %d\n"
     "exit on failure ?               : %s\n"
     "  failure output file name prefix : %s\n"
-    "initial honeycombs to ignore    : %d\n",
+    "initial honeycombs to ignore    : %d\n"),
     COHERENCE_VECTOR == cmdline_args.sim_engine ? "COHERENCE_VECTOR" : "BISTABLE",
     cmdline_args.pszSimOptsFName,         cmdline_args.pszFName,         cmdline_args.pszReferenceSimOutputFName, 
     cmdline_args.number_of_sims,          cmdline_args.dTolerance,       cmdline_args.dThreshLower, 
@@ -124,19 +125,19 @@ int main (int argc, char **argv)
 
   if (!open_project_file (cmdline_args.pszFName, &design))
     {
-    flush_fprintf (stderr, "Failed to open the circuit file !\n") ;
+    flush_fprintf (stderr, _("Failed to open the circuit file !\n")) ;
     return 1 ;
     }
   else
   if (NULL == design)
     {
-    flush_fprintf (stderr, "Failed to open the circuit file !\n") ;
-    return 2 ;
+    flush_fprintf (stderr, _("Failed to open the circuit file !\n")) ;
+    return 1 ;
     }
 
   if (NULL == (sim_output = open_simulation_output_file (cmdline_args.pszReferenceSimOutputFName)))
     {
-    flush_fprintf (stderr, "Failed to open the reference simulation output file !\n") ;
+    flush_fprintf (stderr, _("Failed to open the reference simulation output file !\n")) ;
     return 3 ;
     }
 
@@ -146,7 +147,7 @@ int main (int argc, char **argv)
 
     if (NULL == (bo = open_bistable_options_file (cmdline_args.pszSimOptsFName)))
       {
-      flush_fprintf (stderr, "Failed to open simulation options file !\n") ;
+      flush_fprintf (stderr, _("Failed to open simulation options file !\n")) ;
       return 4 ;
       }
     bistable_options_dump (bo, stderr) ;
@@ -159,14 +160,15 @@ int main (int argc, char **argv)
 
     if (NULL == (co = open_coherence_options_file (cmdline_args.pszSimOptsFName)))
       {
-      flush_fprintf (stderr, "Failed to open simulation options file !\n") ;
+      flush_fprintf (stderr, _("Failed to open simulation options file !\n")) ;
       return 5 ;
       }
     coherence_options_dump (co, stderr) ;
     memcpy (&coherence_options, co, sizeof (coherence_OP)) ;
     }
 
-  printf ("Running %d simulations with a radial tolerance of %lf\n", cmdline_args.number_of_sims, cmdline_args.dTolerance) ;
+  printf (_("Running %d %s with a radial tolerance of %lf\n"), cmdline_args.number_of_sims, 
+    1 == cmdline_args.number_of_sims ? _("simulation") : _("simulations"), cmdline_args.dTolerance) ;
 
   ref_hcs = create_honeycombs_from_buses (sim_output->sim_data, sim_output->bus_layout, QCAD_CELL_OUTPUT, cmdline_args.dThreshLower, cmdline_args.dThreshUpper, cmdline_args.icAverageSamples) ;
 
@@ -182,7 +184,7 @@ int main (int argc, char **argv)
 
   for (Nix = 0 ; Nix < cmdline_args.number_of_sims && !bDie ; Nix++)
     {
-    flush_fprintf (stderr, "Running simulation %d of %d\n", Nix, cmdline_args.number_of_sims) ;
+    flush_fprintf (stderr, _("Running simulation %d of %d\n"), Nix, cmdline_args.number_of_sims) ;
     if (NULL != (working_design = design_copy (design)))
       {
       if (cmdline_args.dTolerance != 0.0)
@@ -199,12 +201,12 @@ int main (int argc, char **argv)
             hcdRef = exp_array_index_1d (ref_hcs, HONEYCOMB_DATA *, Nix1) ;
             hcdOut = exp_array_index_1d (out_hcs, HONEYCOMB_DATA *, Nix1) ;
 
-            flush_fprintf (stderr, "Reference output bus is: (0x%08x)\"%s\"\n", (int)hcdRef, hcdRef->bus->pszName) ;
+            flush_fprintf (stderr, _("Reference output bus is: (0x%08x)\"%s\"\n"), (int)hcdRef, hcdRef->bus->pszName) ;
             for (Nix2 = 0 ; Nix2 < hcdRef->arHCs->icUsed ; Nix2++)
               flush_fprintf (stderr, "%llu ", exp_array_index_1d (hcdRef->arHCs, HONEYCOMB, Nix2).value) ;
             flush_fprintf (stderr, "\n") ;
 
-            flush_fprintf (stderr, "Output bus is: (0x%08x)\"%s\"\n", (int)hcdOut, hcdOut->bus->pszName) ;
+            flush_fprintf (stderr, _("Output bus is: (0x%08x)\"%s\"\n"), (int)hcdOut, hcdOut->bus->pszName) ;
             for (Nix2 = 0 ; Nix2 < hcdOut->arHCs->icUsed ; Nix2++)
               flush_fprintf (stderr, "%llu ", exp_array_index_1d (hcdOut->arHCs, HONEYCOMB, Nix2).value) ;
             flush_fprintf (stderr, "\n") ;
@@ -212,11 +214,11 @@ int main (int argc, char **argv)
             single_success =
               determine_success (hcdRef, hcdOut, cmdline_args.circuit_delay) ;
 
-            flush_fprintf (stderr, "This run[%d] bus[%d] was %ssuccessful\n", Nix, Nix1, 0 == single_success ? "un" : "") ;
+            flush_fprintf (stderr, _("This run[%d] bus[%d] was %ssuccessful\n"), Nix, Nix1, 0 == single_success ? "un" : "") ;
             }
           else
             {
-            flush_fprintf (stderr, "Output has more buses than reference !\n") ;
+            flush_fprintf (stderr, _("Output has more buses than reference !\n")) ;
             single_success = 0 ;
             }
 
@@ -228,12 +230,12 @@ int main (int argc, char **argv)
               SIMULATION_OUTPUT failed_sim_output = {sim_data, design->bus_layout, FALSE} ;
 
               psz = g_strdup_printf ("%s.sim_output", cmdline_args.pszFailureFNamePrefix) ;
-              fprintf (stderr, "Creating %s to record the failure.\n", psz) ;
+              fprintf (stderr, _("Creating %s to record the failure.\n"), psz) ;
               create_simulation_output_file (psz, &failed_sim_output) ;
               g_free (psz) ;
 
               psz = g_strdup_printf ("%s.qca", cmdline_args.pszFailureFNamePrefix) ;
-              fprintf (stderr, "Creating %s to record the failure.\n", psz) ;
+              fprintf (stderr, _("Creating %s to record the failure.\n"), psz) ;
               create_file (psz, working_design) ;
               g_free (psz) ;
               }
@@ -319,19 +321,19 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
 
   for (Nix = 0 ; Nix < argc ; Nix++)
     {
-    if (!(strcmp (argv[Nix], "-a") && strcmp (argv[Nix], "--average")))
+    if (!(strcmp (argv[Nix], _("-a")) && strcmp (argv[Nix], _("--average"))))
       {
       if (++Nix < argc)
         cmdline_args->icAverageSamples = atoi (argv[Nix]) ;
       }
     else
-    if (!(strcmp (argv[Nix], "-d") && strcmp (argv[Nix], "--delay")))
+    if (!(strcmp (argv[Nix], _("-d")) && strcmp (argv[Nix], _("--delay"))))
       {
       if (++Nix < argc)
         cmdline_args->circuit_delay = atoi (argv[Nix]) ;
       }
     else
-    if (!(strcmp (argv[Nix], "-e") && strcmp (argv[Nix], "--engine")))
+    if (!(strcmp (argv[Nix], _("-e")) && strcmp (argv[Nix], _("--engine"))))
       {
       if (++Nix < argc)
         cmdline_args->sim_engine =
@@ -342,7 +344,7 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
               : BISTABLE /* default */ ;
       }
     else
-    if (!(strcmp (argv[Nix], "-f") && strcmp (argv[Nix], "--file")))
+    if (!(strcmp (argv[Nix], _("-f")) && strcmp (argv[Nix], _("--file"))))
       {
       if (++Nix < argc)
         {
@@ -351,13 +353,13 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
         }
       }
     else
-    if (!(strcmp (argv[Nix], "-l") && strcmp (argv[Nix], "--lower")))
+    if (!(strcmp (argv[Nix], _("-l")) && strcmp (argv[Nix], _("--lower"))))
       {
       if (++Nix < argc)
         cmdline_args->dThreshLower = g_ascii_strtod (argv[Nix], NULL) ;
       }
     else
-    if (!(strcmp (argv[Nix], "-n") && strcmp (argv[Nix], "--number")))
+    if (!(strcmp (argv[Nix], _("-n")) && strcmp (argv[Nix], _("--number"))))
       {
       if (++Nix < argc)
         {
@@ -366,7 +368,7 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
         }
       }
     else
-    if (!(strcmp (argv[Nix], "-o") && strcmp (argv[Nix], "--options")))
+    if (!(strcmp (argv[Nix], _("-o")) && strcmp (argv[Nix], _("--options"))))
       {
       if (++Nix < argc)
         {
@@ -375,7 +377,7 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
         }
       }
     else
-    if (!(strcmp (argv[Nix], "-r") && strcmp (argv[Nix], "--results")))
+    if (!(strcmp (argv[Nix], _("-r")) && strcmp (argv[Nix], _("--results"))))
       {
       if (++Nix < argc)
         {
@@ -384,7 +386,7 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
         }
       }
     else
-    if (!(strcmp (argv[Nix], "-t") && strcmp (argv[Nix], "--tolerance")))
+    if (!(strcmp (argv[Nix], _("-t")) && strcmp (argv[Nix], _("--tolerance"))))
       {
       if (++Nix < argc)
         {
@@ -393,16 +395,16 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
         }
       }
     else
-    if (!(strcmp (argv[Nix], "-u") && strcmp (argv[Nix], "--upper")))
+    if (!(strcmp (argv[Nix], _("-u")) && strcmp (argv[Nix], _("--upper"))))
       {
       if (++Nix < argc)
         cmdline_args->dThreshUpper = g_ascii_strtod (argv[Nix], NULL) ;
       }
     else
-    if (!(strcmp (argv[Nix], "-x") && strcmp (argv[Nix], "--exit")))
+    if (!(strcmp (argv[Nix], _("-x")) && strcmp (argv[Nix], _("--exit"))))
       cmdline_args->bExitOnFailure = TRUE ;
     else
-    if (!(strcmp (argv[Nix], "-s") && strcmp (argv[Nix], "--save")))
+    if (!(strcmp (argv[Nix], _("-s")) && strcmp (argv[Nix], _("--save"))))
       {
       if (++Nix < argc)
         cmdline_args->pszFailureFNamePrefix = argv[Nix] ;
@@ -412,23 +414,22 @@ static void parse_cmdline (int argc, char **argv, CMDLINE_ARGS *cmdline_args)
   if (icParms < 5 || bDie)
     {
     printf (
-"Usage: batch_sim options...\n"
-"\n"
-"Options are:\n"
-"  -a  --average   samples       Optional: Number of samples to use for running average. Default is 1.\n"
-"  -d  --delay     honeycombs    Optional: Number of initial honeycombs to ignore because of circuit delay. Default is 0.\n"
-"  -e  --engine    engine        Optional: The simulation engine. One of BISTABLE (default) or COHERENCE_VECTOR.\n"
-"  -f  --file      file          Required: The circuit file.\n"
-"  -l  --lower     polarization  Optional: Lower polarization threshold. Between -1.00 and 1.00. Default is -0.5.\n"
-"  -n  --number    number        Required: Number of simulations to perform.\n"
-"  -o  --options   file          Required: Simulation engine options file.\n"
-"  -r  --results   file          Required: Simulation results file to compare generated results to.\n"
-"  -t  --tolerance tolerance     Required: Radial tolerance. Non-negative floating point value.\n"
-"  -u  --upper     polarization  Optional: Upper polarization threshold. Between -1.00 and 1.00. Default is 0.5.\n"
-"  -x  --exit                    Optional: Turn on exit-on-first-failure.\n"
-"    -s  --save      file_prefix   Optional: Save failing simulation output to file_prefix.sim_output\n"
-"                                            and the circuit to file_prefix.qca.\n"
-) ;
+    _("Usage: batch_sim options...\n"
+      "\n"
+      "Options are:\n"
+      "  -a  --average   samples       Optional: Number of samples to use for running average. Default is 1.\n"
+      "  -d  --delay     honeycombs    Optional: Number of initial honeycombs to ignore because of circuit delay. Default is 0.\n"
+      "  -e  --engine    engine        Optional: The simulation engine. One of BISTABLE (default) or COHERENCE_VECTOR.\n"
+      "  -f  --file      file          Required: The circuit file.\n"
+      "  -l  --lower     polarization  Optional: Lower polarization threshold. Between -1.00 and 1.00. Default is -0.5.\n"
+      "  -n  --number    number        Required: Number of simulations to perform.\n"
+      "  -o  --options   file          Required: Simulation engine options file.\n"
+      "  -r  --results   file          Required: Simulation results file to compare generated results to.\n"
+      "  -t  --tolerance tolerance     Required: Radial tolerance. Non-negative floating point value.\n"
+      "  -u  --upper     polarization  Optional: Upper polarization threshold. Between -1.00 and 1.00. Default is 0.5.\n"
+      "  -x  --exit                    Optional: Turn on exit-on-first-failure.\n"
+      "    -s  --save      file_prefix   Optional: Save failing simulation output to file_prefix.sim_output\n"
+      "                                            and the circuit to file_prefix.qca.\n")) ;
     exit (1) ;
     }
   }
@@ -443,7 +444,7 @@ static int determine_success (HONEYCOMB_DATA *hcdRef, HONEYCOMB_DATA *hcdOut, in
   if (NULL == hcdRef->arHCs || NULL == hcdOut->arHCs) return 0 ;
   if (0 == hcdRef->arHCs->icUsed || 0 == hcdOut->arHCs->icUsed) return 0 ;
 
-  flush_fprintf (stderr, "Determining success for hcdRef = 0x%08x vs. hcdOut = 0x%08x\n", (int)hcdRef, (int)hcdOut) ;
+  flush_fprintf (stderr, _("Determining success for hcdRef = 0x%08x vs. hcdOut = 0x%08x\n"), (int)hcdRef, (int)hcdOut) ;
 
   // If both honeycomb arrays have the same number of honeycombs, compare the values
   if (hcdRef->arHCs->icUsed == hcdOut->arHCs->icUsed)
@@ -453,9 +454,9 @@ static int determine_success (HONEYCOMB_DATA *hcdRef, HONEYCOMB_DATA *hcdOut, in
       hcRef = &exp_array_index_1d (hcdRef->arHCs, HONEYCOMB, Nix) ;
       hcOut = &exp_array_index_1d (hcdOut->arHCs, HONEYCOMB, Nix) ;
       // windoze doesn't seem to be able to format a string containing multiple %llu tokens
-      flush_fprintf (stderr, "Honeycomb value[%d]: ", Nix) ;
-      flush_fprintf (stderr, "reference: %llu ", hcRef->value) ;
-      flush_fprintf (stderr, "output: %llu\n", hcOut->value) ;
+      flush_fprintf (stderr, _("Honeycomb value[%d]: "), Nix) ;
+      flush_fprintf (stderr, _("reference: %llu "), hcRef->value) ;
+      flush_fprintf (stderr, _("output: %llu\n"), hcOut->value) ;
       if (hcRef->value != hcOut->value)
         break ;
       }

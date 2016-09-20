fddb45c1958275f3883eb7df413ff653232a7308
Jake Retallick <retallick.jake@gmail.com>
2016-09-20 16:46:48 -0700

    changed inline to extern inline in matrixlib_3x3.h for GCC 5 support

--- src/matrixlib_3x3.c
+++ src/matrixlib_3x3.c
@@ -30,23 +30,23 @@
 #include		"matrixlib_3x3.h"
 
 inline void complexMatrixMultiplication(complex A[3][3], complex B[3][3], complex result[3][3]){
-	
+
 	result[0][0] = complexAdd(complexAdd(complexMultiply(A[0][0], B[0][0]), complexMultiply(A[0][1], B[1][0])), complexMultiply(A[0][2], B[2][0]));
 	result[0][1] = complexAdd(complexAdd(complexMultiply(A[0][0], B[0][1]), complexMultiply(A[0][1], B[1][1])), complexMultiply(A[0][2], B[2][1]));
 	result[0][2] = complexAdd(complexAdd(complexMultiply(A[0][0], B[0][2]), complexMultiply(A[0][1], B[1][2])), complexMultiply(A[0][2], B[2][2]));
-	
+
 	result[1][0] = complexAdd(complexAdd(complexMultiply(A[1][0], B[0][0]), complexMultiply(A[1][1], B[1][0])), complexMultiply(A[1][2], B[2][0]));
 	result[1][1] = complexAdd(complexAdd(complexMultiply(A[1][0], B[0][1]), complexMultiply(A[1][1], B[1][1])), complexMultiply(A[1][2], B[2][1]));
 	result[1][2] = complexAdd(complexAdd(complexMultiply(A[1][0], B[0][2]), complexMultiply(A[1][1], B[1][2])), complexMultiply(A[1][2], B[2][2]));
-	
+
 	result[2][0] = complexAdd(complexAdd(complexMultiply(A[2][0], B[0][0]), complexMultiply(A[2][1], B[1][0])), complexMultiply(A[2][2], B[2][0]));
 	result[2][1] = complexAdd(complexAdd(complexMultiply(A[2][0], B[0][1]), complexMultiply(A[2][1], B[1][1])), complexMultiply(A[2][2], B[2][1]));
 	result[2][2] = complexAdd(complexAdd(complexMultiply(A[2][0], B[0][2]), complexMultiply(A[2][1], B[1][2])), complexMultiply(A[2][2], B[2][2]));
-	
+
 }
 
 inline void complexMatrixAddition(complex A[3][3], complex B[3][3], complex result[3][3]){
-	
+
 	result[0][0] = complexAdd(A[0][0], B[0][0]);
 	result[0][1] = complexAdd(A[0][1], B[0][1]);
 	result[0][2] = complexAdd(A[0][2], B[0][2]);
@@ -56,12 +56,12 @@ inline void complexMatrixAddition(complex A[3][3], complex B[3][3], complex resu
 	result[2][0] = complexAdd(A[2][0], B[2][0]);
 	result[2][1] = complexAdd(A[2][1], B[2][1]);
 	result[2][2] = complexAdd(A[2][2], B[2][2]);
-	
+
 }
 
 inline void complexMatrixSubtraction(complex A[3][3], complex B[3][3], complex result[3][3]){
-	
-	result[0][0] = complexSub(A[0][0], B[0][0]);	
+
+	result[0][0] = complexSub(A[0][0], B[0][0]);
 	result[0][1] = complexSub(A[0][1], B[0][1]);
 	result[0][2] = complexSub(A[0][2], B[0][2]);
 	result[1][0] = complexSub(A[1][0], B[1][0]);
@@ -70,11 +70,11 @@ inline void complexMatrixSubtraction(complex A[3][3], complex B[3][3], complex r
 	result[2][0] = complexSub(A[2][0], B[2][0]);
 	result[2][1] = complexSub(A[2][1], B[2][1]);
 	result[2][2] = complexSub(A[2][2], B[2][2]);
-	
+
 }
 
 inline void complexConstMatrixMultiplication(complex A, complex B[3][3], complex result[3][3]){
-	
+
 	result[0][0] = complexMultiply(A, B[0][0]);
 	result[0][1] = complexMultiply(A, B[0][1]);
 	result[0][2] = complexMultiply(A, B[0][2]);
@@ -84,11 +84,11 @@ inline void complexConstMatrixMultiplication(complex A, complex B[3][3], complex
 	result[2][0] = complexMultiply(A, B[2][0]);
 	result[2][1] = complexMultiply(A, B[2][1]);
 	result[2][2] = complexMultiply(A, B[2][2]);
-	
+
 }
 
 inline void complexConstMatrixDivision(complex A, complex B[3][3], complex result[3][3]){
-	
+
 	result[0][0] = complexDivide(A, B[0][0]);
 	result[0][1] = complexDivide(A, B[0][1]);
 	result[0][2] = complexDivide(A, B[0][2]);
@@ -98,19 +98,19 @@ inline void complexConstMatrixDivision(complex A, complex B[3][3], complex resul
 	result[2][0] = complexDivide(A, B[2][0]);
 	result[2][1] = complexDivide(A, B[2][1]);
 	result[2][2] = complexDivide(A, B[2][2]);
-	
+
 }
 
 inline void complexMatrixRealExponential(complex result[3][3]){
 	//takes the exponent of the real part
-	//basically we assume that this will be used only on real matricies 
+	//basically we assume that this will be used only on real matricies
 	//that had to be made complex for other calculations
 	//checks if a matrix is real
 	//if(!IS_REAL_3X3(result)){
 	//	printf("Critical Error: complex matrix was passed to complexMatrixRealExponential()\n");
 	//	exit(1);
 	//}
-	
+
 	result[0][0].re = exp(result[0][0].re);	result[0][1].re = exp(result[0][1].re);	result[0][2].re = exp(result[0][2].re);
 	result[1][0].re = exp(result[1][0].re);	result[1][1].re = exp(result[1][1].re);	result[1][2].re = exp(result[1][2].re);
 	result[2][0].re = exp(result[2][0].re);	result[2][1].re = exp(result[2][1].re);	result[2][2].re = exp(result[2][2].re);
@@ -120,17 +120,17 @@ inline void complexMatrixRealExponential(complex result[3][3]){
 // complex matrix trace
 inline complex complexTr(complex A[3][3]){
 	complex result;
-	
+
 	result.re = A[0][0].re + A[1][1].re + A[2][2].re;
 	result.im = A[0][0].im + A[1][1].im + A[2][2].im;
-	
+
 	return(result);
 }
 
 //complex multiplication using only three real multiplications
 inline complex complexMultiply(complex A, complex B){
 	/*complex result;
-	
+
 	result.re = A.re*B.re - A.im*B.im;
 	result.im = A.re*B.im + A.im*B.re;
 	return result;
@@ -138,11 +138,11 @@ inline complex complexMultiply(complex A, complex B){
 	complex result;
 	double temp = A.re*B.re;
 	double temp2 = A.im*B.im;
-	
+
 	result.re = temp - temp2;
 	result.im = (A.re+A.im)*(B.re+B.im) - temp - temp2;
 	return result;
-	
+
 }
 
 inline complex complexDivide(complex denominator, complex numerator){
@@ -152,7 +152,7 @@ inline complex complexDivide(complex denominator, complex numerator){
 	long double dre = denominator.re;
 	long double dim = denominator.im;
 	long double denom;
-	
+
 	//if numbers are too large they could cause an overflow in the division
 	//so instead I multiply the numerator and denominator by a very small number
 	//the result should be the same. However .....
@@ -163,9 +163,9 @@ inline complex complexDivide(complex denominator, complex numerator){
 		dre*=1e-150;
 		dim*=1e-150;
 		}
-		
+
 	denom = dre*dre + dim*dim;
-	
+
 	result.re = (long double)((nre*dre+nim*dim)/denom);
 	result.im = (long double)((nim*dre-nre*dim)/denom);
 	return result;
@@ -173,7 +173,7 @@ inline complex complexDivide(complex denominator, complex numerator){
 
 inline complex complexAdd(complex A, complex B){
 	complex result;
-	
+
 	result.re = A.re + B.re;
 	result.im = A.im + B.im;
 	return result;
@@ -181,14 +181,14 @@ inline complex complexAdd(complex A, complex B){
 
 inline complex complexSub(complex A, complex B){
 	complex result;
-	
+
 	result.re = A.re - B.re;
 	result.im = A.im - B.im;
 	return result;
 }
 
 inline void complexIdentityMatrix(complex result[3][3]){
-	
+
 	result[0][0].re = 1;
 	result[0][1].re = 0;
 	result[0][2].re = 0;
@@ -198,7 +198,7 @@ inline void complexIdentityMatrix(complex result[3][3]){
 	result[2][0].re = 0;
 	result[2][1].re = 0;
 	result[2][2].re = 1;
-	
+
 	result[0][0].im = 0;
 	result[0][1].im = 0;
 	result[0][2].im = 0;
@@ -208,7 +208,7 @@ inline void complexIdentityMatrix(complex result[3][3]){
 	result[2][0].im = 0;
 	result[2][1].im = 0;
 	result[2][2].im = 0;
-		
+
 }
 
 inline void complexExtractColumn(int column, complex A[3][3], complex vector[3]){
--- src/matrixlib_3x3.h
+++ src/matrixlib_3x3.h
@@ -23,27 +23,30 @@
 //                                                      //
 //////////////////////////////////////////////////////////
 
+#ifndef MATRIXLIB_3X3_H_
+#define MATRIXLIB_3X3_H_
+
 typedef struct
 {
 	double re;
 	double im;
 }complex;
 
-inline void complexCheckRealMatrix(complex A[3][3]);
-inline complex complexMultiply(complex A, complex B);
-inline complex complexDivide(complex denominator, complex numerator);
-inline complex complexAdd(complex A, complex B);
-inline complex complexSub(complex A, complex B);
-inline void complexConstMatrixMultiplication(complex A, complex B[3][3], complex result[3][3]);
-inline void complexConstMatrixDivision(complex A, complex B[3][3], complex result[3][3]);
-inline void complexMatrixMultiplication(complex A[3][3], complex B[3][3], complex result[3][3]);
-inline void complexMatrixAddition(complex A[3][3], complex B[3][3], complex result[3][3]);
-inline void complexMatrixSubtraction(complex A[3][3], complex B[3][3], complex result[3][3]);
-inline void complexMatrixRealExponential(complex result[3][3]);
-inline complex complexTr(complex A[3][3]);
-inline void complexIdentityMatrix(complex result[3][3]);
-inline void complexExtractRow(int row, complex A[3][3], complex vector[3]);
-inline void complexExtractColumn(int column, complex A[3][3], complex vector[3]);
+//inline void complexCheckRealMatrix(complex A[3][3]);
+extern inline complex complexMultiply(complex A, complex B);
+extern inline complex complexDivide(complex denominator, complex numerator);
+extern inline complex complexAdd(complex A, complex B);
+extern inline complex complexSub(complex A, complex B);
+extern inline void complexConstMatrixMultiplication(complex A, complex B[3][3], complex result[3][3]);
+extern inline void complexConstMatrixDivision(complex A, complex B[3][3], complex result[3][3]);
+extern inline void complexMatrixMultiplication(complex A[3][3], complex B[3][3], complex result[3][3]);
+extern inline void complexMatrixAddition(complex A[3][3], complex B[3][3], complex result[3][3]);
+extern inline void complexMatrixSubtraction(complex A[3][3], complex B[3][3], complex result[3][3]);
+extern inline void complexMatrixRealExponential(complex result[3][3]);
+extern inline complex complexTr(complex A[3][3]);
+extern inline void complexIdentityMatrix(complex result[3][3]);
+extern inline void complexExtractRow(int row, complex A[3][3], complex vector[3]);
+extern inline void complexExtractColumn(int column, complex A[3][3], complex vector[3]);
 
 #define IS_REAL_3X3(A) \
 	(0 == (A)[0][0].im &&  \
@@ -55,3 +58,5 @@ inline void complexExtractColumn(int column, complex A[3][3], complex vector[3])
 	 0 == (A)[2][0].im &&  \
 	 0 == (A)[2][1].im &&  \
 	 0 == (A)[2][2].im)
+
+#endif
